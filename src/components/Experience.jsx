60ca794acbbe798f2d64fcd433a6bcc92fbfb90b
SolomonWole <solomonwolemail@gmail.com>
2022-12-19 14:01:59 +0100

    comit

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -16,7 +16,7 @@ const Experience = () => {
           <ResumeTemplate resumes={resumes} />
         </div>
         
-       <a href="/src/assets/Solomon Portfolio.pdf" download> <StyledFooterButton>
+       <a href="/public/Solomon_Portfolio.pdf" download> <StyledFooterButton>
             Donwload Resume &nbsp; <AiOutlineDownload className="icon"/>
         </StyledFooterButton></a>
       </StyledExp>
